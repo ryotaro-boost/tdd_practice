953d42d6c61775308ed75e9c6477d00da5c28058
ryotaro-boost <ryotaro.t.0727@gmail.com>
2020-11-14 20:45:26 +0000

    冗長な条件分岐を修正

--- app/Models/VacancyLevel.php
+++ app/Models/VacancyLevel.php
@@ -15,13 +15,11 @@ class VacancyLevel extends Model
 
     public function mark(): string
     {
-        if ($this->remainingCount === 0) {
-            return '×';
-        }
-        if ($this->remainingCount < 5) {
-            return '△';
-        }
-        return '◎';
+        $marks = ['empty' => '×', 'few' => '△', 'enough' => '◎'];
+        $slug = $this->slug();
+        assert(isset($marks[$slug]), new \DomainException('invalid slug value.'));
+
+        return $marks[$slug];
     }
 
     public function slug(): string
