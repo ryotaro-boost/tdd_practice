cad504252fb18fcf21bbcf52e1adfa8b48dd3671
ryotaro-boost <ryotaro.t.0727@gmail.com>
2020-11-14 22:21:31 +0000

    testを記述

--- tests/Feature/Http/Controllers/LessonControllerTest.php
+++ tests/Feature/Http/Controllers/LessonControllerTest.php
@@ -3,20 +3,53 @@
 namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Lesson;
+use App\Models\User;
+use App\Models\Reservation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class LessonControllerTest extends TestCase
 {
-    public function testShow()
+    /**
+     * @param int $reservationCount
+     * @param int $capacity
+     * @param string $expectedVacancyLevelMark
+     * @dataProvider dataShow
+     */
+    public function testShow(int $reservationCount, int $capacity, string $expectedVacancyLevelMark)
     {
-        $lesson = factory(Lesson::class)->create(['name' => '楽しいヨガレッスン']);
-
+        $lesson = factory(Lesson::class)->create(['name' => '楽しいヨガレッスン', 'capacity' => $capacity]);
+        for ($i =0;$i<$reservationCount;$i++) {
+            $user = factory(User::class)->create();
+            $lesson->reservations()->save(factory(Reservation::class)->make(['user_id' => $user]));
+        }
         $response = $this->get("/lessons/{$lesson->id}");
 
         $response->assertStatus(Response::HTTP_OK);
         $response->assertSeeText($lesson->name);
-        $response->assertSeeText('空き状況:×');
+        $response->assertSeeText('空き状況:{$expectedVacancyLevelMark}');
+    }
+
+    public function dataShow()
+    {
+        return [
+            '空きなし' => [
+                'reservationCount' => 10,
+                'capacity' => 10,
+                '$expectedVacancyLevelMark' => '×',
+            ],
+            '残りわずか' => [
+                'reservationCount' => 6,
+                'capacity' => 10,
+                '$expectedVacancyLevelMark' => '△',
+            ],
+            '空き十分' => [
+                'reservationCount' => 1,
+                'capacity' => 10,
+                '$expectedVacancyLevelMark' => '◎',
+            ],
+        ];
+        
     }
 }
