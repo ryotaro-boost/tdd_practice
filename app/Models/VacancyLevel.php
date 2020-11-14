ca29a9b47e5ff135a91ad476ef1cb491fa0e41c6
ryotaro-boost <ryotaro.t.0727@gmail.com>
2020-11-14 20:02:00 +0000

    testを通す

---
+++ app/Models/VacancyLevel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VacancyLevel extends Model
+{
+    private $remainingCount;
+
+    public function  __construct(int $remainingCount)
+    {
+        $this->remainingCount = $remainingCount;
+    }
+
+    public function mark(): string
+    {
+        if ($this->remainingCount === 0) {
+            return '×';
+        }
+        if ($this->remainingCount < 5) {
+            return '△';
+        }
+        return '◎';
+    }
+}
